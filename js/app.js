eb1cdb40613e736dd1a9189d1dedea125236242c
Onida <OnidaSurshana@gmail.com>
2019-07-11 14:28:34 -0400

    js/app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 "use strict";
-alert("Welcome. Please answer these 5 trivia questions about me! Type 'yes' or 'no' to answer. (Y or N also work)");
-var name = prompt("What is your name?");
+// alert("Welcome. Please answer these 5 trivia questions about me! Type 'yes' or 'no' to answer. (Y or N also work)");
+// var name = prompt("What is your name?");
 var score = 0;
 // //
-// //QUESTION1
+var questions = [
+  "Does my family call me Maria?",
+  "Is my favorite animal one that I have never seen?",
+  "Do I think bugs are cute?",
+  "Do I like watching sports?",
+  "Do I like participating in sports?"
+];
+var answers = ["no","yes","no","no","yes"];
 function quiz(question, answer) {
   var response = prompt(question).toLowerCase();
   console.log(question);
@@ -17,77 +24,10 @@ function quiz(question, answer) {
     alert("Good try, but you got it wrong.");
   }
 }
-quiz("do i like cheese?", "no");
+for(var i = 0; i < questions.length; i++) {
+  quiz(questions[i], answers[i]);
+}
 
-// var question1 = "Does my family call me Maria?";
-// var response1 = prompt(question1).toLowerCase();
-// console.log(question1);
-// console.log("User responded " + response1);
-// if(response1 === "n" || response1 === "no") {
-//   console.log("That is correct");
-//   score +=1;
-//   alert("Good job! You got it right!");
-// } else {
-//   console.log("That is incorrect");
-//   alert("Good try, but you got it wrong.");
-// }
-// //
-// //QUESTION2
-// var question2 = "Is my favorite animal one that I have never seen?";
-// var response2 = prompt(question2).toLowerCase();
-// console.log(question2);
-// console.log("User responded " + response2);
-// if(response2 === "y" || response2 === "yes") {
-//   console.log("That is correct");
-//   score +=1;
-//   alert("Good job! You got it right!");
-// } else {
-//   console.log("That is incorrect");
-//   alert("Good try, but you got it wrong.");
-// }
-// //
-// //QUESTION3
-// var question3 = "Do I think bugs are cute?";
-// var response3 = prompt(question3).toLowerCase();
-// console.log(question3);
-// console.log("User responded " + response3);
-// if(response3 === "n" || response3 === "no") {
-//   console.log("That is correct");
-//   score +=1;
-//   alert("Good job! You got it right!");
-// } else {
-//   console.log("That is incorrect");
-//   alert("Good try, but you got it wrong.");
-// }
-// //
-// //QUESTION4
-// var question4 = "Do I like watching sports?";
-// var response4 = prompt(question4).toLowerCase();
-// console.log(question4);
-// console.log("User responded " + response4);
-// if(response4 === "n" || response4 === "no") {
-//   console.log("That is correct");
-//   score +=1;
-//   alert("Good job! You got it right!");
-// } else {
-//   console.log("That is incorrect");
-//   alert("Good try, but you got it wrong.");
-// }
-// //
-// //QUESTION 5
-// var question5 = "Do I like participating in sports?";
-// var response5 = prompt(question5).toLowerCase();
-// console.log(question5);
-// console.log("User responded " + response5);
-// if(response5 === "y" || response5 === "yes") {
-//   console.log("That is correct");
-//   score +=1;
-//   alert("Good job! You got it right!");
-// } else {
-//   console.log("That is incorrect");
-//   alert("Good try, but you got it wrong.");
-// }
-// //
 // //Question 6
 // var question6 = "At what age did I first do a handstand?";
 // var answer6 = 25;
